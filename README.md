157bbdc788d1d373e170af8105e748982699a9f7
neeswebservices <techneesofficial@gmail.com>
2022-09-09 16:52:41 +0545

    mdchange

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![Version](https://img.shields.io/npm/v/react-number-increment)](https://www.npmjs.com/package/react-number-increment)
 [![Downloads](https://img.shields.io/npm/dy/react-number-increment)](http://www.npmtrends.com/react-number-increment)
 
-A configurable React component For counting up the number in React appilcation. [react-number-increment](https://github.com/neeswebservices/react-num-counter.git).
+A configurable React component For counting up the number in your React appilcation. [react-number-increment](https://github.com/neeswebservices/react-num-counter.git).
 
 ## Table of Contents
 
@@ -21,7 +21,7 @@ yarn add react-number-increment
 npm install react-number-increment
 ```
 
-## Usage
+## Import
 
 ```js
 import Counter from "react-number-increment";
@@ -39,18 +39,18 @@ This will start a count up transition from `0` to `100` on render.
 
 ```js
 // import "./App.css";
-import Counter from "./Counter/index";
+import Counter from "react-number-increment";
 
 function App() {
   return (
     <div className="App">
       <Counter
-        start=0 // number for starting
-        end=500 // where it ends
-        speed=0.1 // speed of increment 0.1 is the fastest
+        start=0 // starting
+        end=500 // ends
+        speed=0.1 // speed / delay
         style={{ fontSize: "200px", fontWeight: 900 }} // add custom style
         decrement // in case if you want to decrement
-        triggeronce // once updates on render
+        triggeronce // once updates on render | NA
         threshold=1 // viewport
         child="+" // add childs ie + , - , *
       />
@@ -66,11 +66,9 @@ Note that `offsetend={4}` will add the offset to the ending value.
 #### Add end and starting Number
 
 ```js
-<CountUp offsetend={4} speed={10} end={10} />
+<Counter offsetend={4} speed={10} end={10} />
 ```
 
 If you have end number 100 it will ends in 104.Right now triggeronce on underdevelopment you may have to wait for another version for working of It.
 
-## License
-
-MIT
+##### License MIT
---
+++ dist/index.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+/**
+ * 描述
+ * @date 2022-09-09
+ * @param {number} {start=0
+ * @param {number} end=100
+ * @param {number} speed=1
+ * @param {boolean} decrement
+ * @param {number} offsetend=0
+ * @param {any} style
+ * @param {boolean} triggeronce=false
+ * @param {number} threshold=0.5
+ * @param {any} child
+ * @param {any} }
+ * @returns {any}
+ */
+
+const Counter = ({
+  start = 0,
+  end = 100,
+  speed = 1,
+  decrement,
+  offsetend = 0,
+  style,
+  triggeronce = false,
+  threshold = 0.5,
+  child
+}) => {
+  const [count, setCount] = useState(null);
+  const [inView, setInview] = useState(false);
+  const countref = useRef(start);
+  const countdown = useRef(end);
+  const [trigger, setTrigger] = useState(null);
+  const viewRef = useRef();
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      const entry = entries[0];
+      setInview(entry.isIntersecting);
+    }, {
+      threshold
+    });
+    observer.observe(viewRef.current); /// function to be triggered
+
+    const incrementCounter = () => {
+      const accumulator = end / 100;
+
+      if (countref.current < end) {
+        const result = Math.ceil(countref.current + accumulator);
+
+        if (result >= end) {
+          //   setTrigger(true);
+          return setCount(end + offsetend);
+        }
+
+        setCount(result);
+        countref.current = result;
+      }
+
+      if (trigger) {
+        clearTimeout(incrementCounter);
+        setCount(end);
+      }
+
+      setTimeout(incrementCounter, speed * 50);
+    };
+
+    const decrementCounter = () => {
+      const accumulator = end / 100;
+
+      if (countdown.current > start) {
+        const result = Math.ceil(countdown.current - accumulator);
+
+        if (result <= start) {
+          setTrigger(false);
+          return setCount(start + offsetend);
+        }
+
+        setCount(result);
+        countdown.current = result;
+      }
+
+      if (trigger) {
+        clearTimeout(decrementCounter);
+        setCount(start);
+      } else {
+        setTimeout(decrementCounter, speed * 50);
+      }
+    };
+
+    if (inView) {
+      if (decrement) {
+        decrementCounter();
+      } else {
+        incrementCounter();
+      }
+    }
+  }, [inView, start, end, speed, decrement, offsetend, trigger, triggeronce, threshold]);
+  return /*#__PURE__*/React.createElement("div", {
+    ref: viewRef
+  }, /*#__PURE__*/React.createElement("span", {
+    style: style
+  }, count, child));
+};
+
+export default Counter;
